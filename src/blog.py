23f7a84d1131e049257bf824cc6ac06b181c6bcf
saraduponchelle-wq <saraduponchelle@gmail.com>
2026-03-01 17:42:33 +0000

    dawadw

--- src/blog.py
+++ src/blog.py
@@ -7,7 +7,7 @@ import os
 BLOG_CHANNEL_ID = int(os.getenv("BLOG_CHANNEL_ID"))
 BLOG_REVIEW_CHANNEL_ID = int(os.getenv("BLOG_REVIEW_CHANNEL_ID"))
 ADMIN_ROLE_ID = int(os.getenv("ADMIN_ROLE_ID"))
-EMOJI_RANDOM = str(os.getenv("NOTI"))
+EMOJI_NOTI = str(os.getenv("NOTI"))
 
 from src.blog_notifications import get_users_with_news_enabled
 
