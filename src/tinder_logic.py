0ac51959d4aed244ad659e697a29eb26473bb40c
saraduponchelle-wq <saraduponchelle@gmail.com>
2026-03-16 02:32:47 +0000

    sfeseffse

--- src/tinder_logic.py
+++ src/tinder_logic.py
@@ -3,6 +3,13 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+EMOJI_GOLDNOTI = str(os.getenv("GOLDNOTI"))
+EMOJI_HEART = str(os.getenv("HEART"))
+EMOJI_BROKENHEART = str(os.getenv("BROKENHEART"))
+
+EMOJI_BOTON_HEART = discord.PartialEmoji.from_str("<a:heart:1477738562433581338>")
+EMOJI_BOTON_BROKENHEART = discord.PartialEmoji.from_str("<:brokenheart:1477739060423299202>")
+
 
 async def get_connection():
     return await asyncpg.connect(DATABASE_URL)
@@ -114,7 +121,7 @@ async def send_match(user: discord.User, profile_data: dict, other_user: discord
 
     embed = create_profile_embed(profile_data, other_user, show_discord=True)
 
-    embed.title = f"❤️ ¡Has hecho match con {profile_data['name']}!"
+    embed.title = f"{EMOJI_BOTON_HEART} ¡Has hecho match con {profile_data['name']}!"
 
     await user.send(embed=embed)
 
@@ -141,7 +148,7 @@ class LikeBackView(discord.ui.View):
         self.discord_user = discord_user
     
     # ❤️ LIKE BACK
-    @discord.ui.button(label="❤️ Like Back", style=discord.ButtonStyle.success)
+    @discord.ui.button(label=f"{EMOJI_BOTON_HEART} Match", style=discord.ButtonStyle.success)
     async def like_back(self, interaction: discord.Interaction, button: discord.ui.Button):
 
         user1 = interaction.user
@@ -162,21 +169,21 @@ class LikeBackView(discord.ui.View):
         await add_match_stat(user1.id, user2.id)
 
         await interaction.response.edit_message(
-            content="❤️ ¡Es un match!",
+            content=f"{EMOJI_BOTON_HEART} ¡Es un match!",
             view=None
         )
     
     # ❌ RECHAZAR
-    @discord.ui.button(label="❌ Rechazar", style=discord.ButtonStyle.danger)
+    @discord.ui.button(label=f"{EMOJI_BROKENHEART} Rechazar", style=discord.ButtonStyle.danger)
     async def reject(self, interaction: discord.Interaction, button: discord.ui.Button):
     
         await interaction.response.edit_message(
-            content="💔 Has rechazado el like.",
+            content=f"{EMOJI_BROKENHEART} Has rechazado el like.",
             view=None
         )
     
     # 📖 VER BLOGS
-    @discord.ui.button(label="📖 Ver Blogs", style=discord.ButtonStyle.secondary)
+    @discord.ui.button(label="Blogs", style=discord.ButtonStyle.primary)
     async def view_blogs(self, interaction: discord.Interaction, button: discord.ui.Button):
 
         from src.blog_viewer import BlogViewer
@@ -204,7 +211,7 @@ class LikeBackView(discord.ui.View):
         )
     
     # 🚫 BLOQUEAR
-    @discord.ui.button(label="🚫 Bloquear", style=discord.ButtonStyle.danger)
+    @discord.ui.button(label="🚫 Bloquear", style=discord.ButtonStyle.secondary)
     async def block(self, interaction: discord.Interaction, button: discord.ui.Button):
     
         conn = await get_connection()
